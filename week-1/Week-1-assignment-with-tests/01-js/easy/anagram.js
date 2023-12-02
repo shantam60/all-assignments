a5cd471bdaffc4f7a7c99be1dc5725f968280393
shantam60 <129108528+shantam60@users.noreply.github.com>
2023-12-03 01:23:14 +0530

    Update anagram.js

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -7,8 +7,21 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+function isAnagram(str1,str2){
+    const clnstr1=str1.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+    const clnstr2=str2.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+    if(clnstr1.length!==clnstr2.length){
+        return false;
+    }
+    return clnstr1.split('').sort().join('')===clnstr2.split('').sort().join('');
+//split array of characters me convert karta hai then usko lexicographically sort karta hai after that join karta to form a string of characters 
 
 }
+
+console.log(isAnagram("anagram","nagaram"));
+console.log(isAnagram("rat","car"));
+console.log(isAnagram("awesome","awesom"));
+console.log(isAnagram("qwerty","qeywrt"));
+
 
 module.exports = isAnagram;
